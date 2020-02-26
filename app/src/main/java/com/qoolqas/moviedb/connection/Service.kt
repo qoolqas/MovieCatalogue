4b9ac2478f2619ff2610d67155e135f63575343f
Qoolqas <qoolqas@gmail.com>
2020-02-26 15:52:44 +0700

    model

--- app/src/main/java/com/qoolqas/moviedb/connection/Service.kt
+++ app/src/main/java/com/qoolqas/moviedb/connection/Service.kt
@@ -4,17 +4,40 @@ import com.qoolqas.moviedb.model.genre.GenreMovieResponse
 import com.qoolqas.moviedb.model.popular.PopularMovieResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface Service {
+    //region Popular
     @GET("movie/popular")
     fun getPopular(
         @Query("api_key") api: String,
         @Query("page") page: Int
-    ):Call<PopularMovieResponse>
+    ): Call<PopularMovieResponse>
 
+    //endregion
+
+    //region Discover
+    @GET("movie/popular")
+    fun getDiscover(
+        @Query("api_key") api: String,
+        @Query("page") page: Int
+    ): Call<PopularMovieResponse>
+    //endregion
+
+    //region Details
+    @GET("movie/popular")
+    fun getDetails(
+        @Query("api_key") api: String,
+        @Path("id") id: Int
+    ): Call<PopularMovieResponse>
+    //endregion
+
+    //region Genre
     @GET("genre/movie/list")
     fun getGenre(
         @Query("api_key") api: String
-    ):Call<GenreMovieResponse>
+    ): Call<GenreMovieResponse>
+    //endregion
+
 }

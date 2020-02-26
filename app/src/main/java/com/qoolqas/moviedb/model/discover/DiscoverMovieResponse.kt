b511f11bbcbfa6add3ff54d225bbb6bb3e6acfc8
Qoolqas <qoolqas@gmail.com>
2020-02-26 17:47:10 +0700

    Viewmodel

--- app/src/main/java/com/qoolqas/moviedb/model/discover/DiscoverMovieResponse.kt
+++ app/src/main/java/com/qoolqas/moviedb/model/discover/DiscoverMovieResponse.kt
@@ -4,15 +4,15 @@ import com.google.gson.annotations.SerializedName
 
 data class DiscoverMovieResponse(
 
-	@field:SerializedName("page")
+    @field:SerializedName("page")
 	val page: Int? = null,
 
-	@field:SerializedName("total_pages")
+    @field:SerializedName("total_pages")
 	val totalPages: Int? = null,
 
-	@field:SerializedName("results")
-	val results: List<ResultsItem>? = null,
+    @field:SerializedName("results")
+	val results: List<DiscoverResultsItem>? = null,
 
-	@field:SerializedName("total_results")
+    @field:SerializedName("total_results")
 	val totalResults: Int? = null
 )
--- app/src/main/java/com/qoolqas/moviedb/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/qoolqas/moviedb/ui/favorite/FavoriteViewModel.kt
@@ -6,8 +6,5 @@ import androidx.lifecycle.ViewModel
 
 class FavoriteViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
-    }
-    val text: LiveData<String> = _text
+
 }

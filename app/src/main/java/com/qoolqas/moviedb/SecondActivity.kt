5a5f7ceae3a15f8450093113fec46a7f1fdb9f55
Qoolqas <qoolqas@gmail.com>
2020-02-26 13:58:12 +0700

    Make popular recyclerview inside home

--- app/src/main/java/com/qoolqas/moviedb/SecondActivity.kt
+++ app/src/main/java/com/qoolqas/moviedb/SecondActivity.kt
@@ -1,38 +1,39 @@
-package com.qoolqas.moviedb
-
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.qoolqas.moviedb.model.popular.PopularResultsItem
-
-import com.qoolqas.moviedb.ui.home.PopularViewmodel
-import com.qoolqas.moviedb.ui.home.PopularAdapter
-import kotlinx.android.synthetic.main.activity_second.*
-
-class SecondActivity : AppCompatActivity() {
-
-    private lateinit var viewmodel: PopularViewmodel
-    private lateinit var adapter: PopularAdapter
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
-
-        rv.setHasFixedSize(true)
-        rv.layoutManager = LinearLayoutManager(this)
-
-        viewmodel = ViewModelProviders.of(this).get(PopularViewmodel::class.java)
-        viewmodel.init(1)
-        viewmodel.livePopular().observe(this, Observer { popular ->
-            initRv(popular)
-        })
-    }
-
-    private fun initRv(popular: List<PopularResultsItem>) {
-        adapter = PopularAdapter(popular)
-        rv.adapter = adapter
-
-    }
-}
+//package com.qoolqas.moviedb
+//
+//import androidx.appcompat.app.AppCompatActivity
+//import android.os.Bundle
+//import androidx.lifecycle.Observer
+//import androidx.lifecycle.ViewModelProviders
+//import androidx.recyclerview.widget.LinearLayoutManager
+//import com.qoolqas.moviedb.model.popular.PopularResultsItem
+//
+//import com.qoolqas.moviedb.ui.home.PopularViewmodel
+//import com.qoolqas.moviedb.ui.home.PopularAdapter
+//import kotlinx.android.synthetic.main.activity_second.*
+//import kotlinx.android.synthetic.main.fragment_home.*
+//
+//class SecondActivity : AppCompatActivity() {
+//
+//    private lateinit var viewmodel: PopularViewmodel
+//    private lateinit var adapter: PopularAdapter
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_second)
+//
+//        rv.setHasFixedSize(true)
+//        rv.layoutManager = LinearLayoutManager(this)
+//
+//        viewmodel = ViewModelProviders.of(this).get(PopularViewmodel::class.java)
+//        viewmodel.init(1)
+//        viewmodel.livePopular().observe(this, Observer { popular ->
+//            initRv(popular)
+//        })
+//    }
+//
+//    private fun initRv(popular: List<PopularResultsItem>) {
+//        adapter = PopularAdapter(popular)
+//        rv.adapter = adapter
+//
+//    }
+//}
--- app/src/main/java/com/qoolqas/moviedb/ui/home/HomeFragment.kt
+++ app/src/main/java/com/qoolqas/moviedb/ui/home/HomeFragment.kt
@@ -4,28 +4,47 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.qoolqas.moviedb.R
+import com.qoolqas.moviedb.model.popular.PopularResultsItem
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var popularViewModel: PopularViewmodel
+    private lateinit var adapter: PopularAdapter
+    private var linearLayoutManager: LinearLayoutManager =
+        LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+    private var popularRv: RecyclerView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+        val v = inflater.inflate(R.layout.fragment_home, container, false)
+
+        popularRv = v.findViewById(R.id.popularRv)
+        popularRv?.setHasFixedSize(true)
+        popularRv?.layoutManager = linearLayoutManager
+        setHasOptionsMenu(true)
+
+        popularViewModel = ViewModelProviders.of(this).get(PopularViewmodel::class.java)
+        popularViewModel.init(1)
+        popularViewModel.livePopular().observe(viewLifecycleOwner, Observer { popular ->
+            initRv(popular)
         })
-        return root
+
+        return v
+    }
+
+    private fun initRv(popular: List<PopularResultsItem>) {
+        adapter = PopularAdapter(popular)
+        popularRv?.adapter = adapter
+
     }
 }

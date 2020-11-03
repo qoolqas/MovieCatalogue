529567eba866c063c7badf278676f30c30e63ca3
Qoolqas <qoolqas@gmail.com>
2020-11-03 19:31:44 +0700

    data for static Genre

--- app/src/main/java/com/qoolqas/moviedb/ui/discover/genre/GenreFragment.kt
+++ app/src/main/java/com/qoolqas/moviedb/ui/discover/genre/GenreFragment.kt
@@ -1,15 +1,36 @@
 package com.qoolqas.moviedb.ui.discover.genre
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.qoolqas.moviedb.R
+import com.qoolqas.moviedb.model.genrestatic.GenreStaticData
 
 
 class GenreFragment : Fragment() {
-
+    private val genreData = listOf(
+        GenreStaticData(R.drawable.exposter, "Action",28),
+        GenreStaticData(R.drawable.exposter, "Adventure",12),
+        GenreStaticData(R.drawable.exposter, "Animation",16),
+        GenreStaticData(R.drawable.exposter, "Comedy",35),
+        GenreStaticData(R.drawable.exposter, "Crime",80),
+        GenreStaticData(R.drawable.exposter, "Documentary",99),
+        GenreStaticData(R.drawable.exposter, "Drama",18),
+        GenreStaticData(R.drawable.exposter, "Family",10751),
+        GenreStaticData(R.drawable.exposter, "Fantasy",14),
+        GenreStaticData(R.drawable.exposter, "History",36),
+        GenreStaticData(R.drawable.exposter, "Horror",27),
+        GenreStaticData(R.drawable.exposter, "Music",10402),
+        GenreStaticData(R.drawable.exposter, "Mystery",9648),
+        GenreStaticData(R.drawable.exposter, "Romance",10749),
+        GenreStaticData(R.drawable.exposter, "Science Fiction",878),
+        GenreStaticData(R.drawable.exposter, "TV Movie",10770),
+        GenreStaticData(R.drawable.exposter, "Thriller",53),
+        GenreStaticData(R.drawable.exposter, "War",10752),
+        GenreStaticData(R.drawable.exposter, "Western",37)
+    )
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -19,5 +40,7 @@ class GenreFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_genre, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
